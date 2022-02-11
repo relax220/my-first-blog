8521187a9cdfad5f2d2ca65025aa1cbca4b83155
A M <artemmochalov220@gmail.com>
2022-02-12 00:38:59 +0400

    Third

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'title', 'created_date', 'published_date')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'text')
+    list_editable = ('published_date',)
+    list_filter = ('published_date', 'id',)
+
+
+admin.site.register(Post, PostAdmin)
